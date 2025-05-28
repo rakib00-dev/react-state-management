bfa6172415688c8e9452f6a72fc0b9f561549772
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-28 15:58:19 +0600

    updated the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { useTokenContext } from './context/Context';
-import { Increase } from './context-components/Increase';
-import { Decrease } from './context-components/Decrease';
+// import { useTokenContext } from './context/Context';
+// import { Increase } from './context-components/Increase';
+// import { Decrease } from './context-components/Decrease';
 
 const App = () => {
-  const { count } = useTokenContext();
-  console.log(count);
+  // const { count } = useTokenContext();
+  // console.log(count);
+  // return (
+  //   <>
+  //     {' '}
+  //     <div>{count} App</div>
+  //     <br />
+  //     <br />
+  //     show <b>Increased</b> Ammount : <Increase />
+  //     <br />
+  //     <br />
+  //     show <b>Decrase</b> Ammount : <Decrease />
+  //   </>
+  // );
 
   return (
     <>
-      {' '}
-      <div>{count} App</div>
-      <br />
-      <br />
-      show <b>Increased</b> Ammount : <Increase />
-      <br />
-      <br />
-      show <b>Decrase</b> Ammount : <Decrease />
+      <h1>hello</h1>
     </>
   );
 };
