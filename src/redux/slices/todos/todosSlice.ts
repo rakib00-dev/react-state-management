582a2dcdaceb47ab83273635a07555d7b9d8d134
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-29 09:26:35 +0600

    added slice for todos

---
+++ src/redux/slices/todos/todosSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const todosSlice = createSlice({
+  initialState: { todos: [{ id: '1', text: 'hello world' }] },
+  name: 'todos',
+  reducers: {
+    addTodos: (state, action) => {
+      const todo = { id: nanoid(), text: action.payload };
+      state.todos.push(todo);
+    },
+    removeTodos: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addTodos, removeTodos } = todosSlice.actions;
+export default todosSlice.reducer;
