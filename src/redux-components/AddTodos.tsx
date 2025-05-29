1a4bebb20f255700ce9c42a345b6ba42d98f259e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-29 20:17:36 +0600

    added the adding and deleting functionality

--- src/redux-components/AddTodos.tsx
+++ src/redux-components/AddTodos.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import { useAppDispatch } from '../redux/hooks';
+import { addTodos } from '../redux/slices/todos/todosSlice';
+
 const AddTodos = () => {
+  const [inputValue, setInputValue] = useState('');
+  const dispatch = useAppDispatch();
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input type="text" />
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        return dispatch(addTodos(inputValue));
+      }}
+    >
+      <input type="text" onChange={(e) => setInputValue(e.target.value)} />
       <button type="submit">Add Todo</button>
     </form>
   );
--- src/redux-components/Todos.tsx
+++ src/redux-components/Todos.tsx
@@ -1,7 +1,9 @@
-import { useAppSelector } from '../redux/hooks';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { removeTodos } from '../redux/slices/todos/todosSlice';
 
 export const Todos = () => {
   const todos = useAppSelector((state) => state.todos.todos);
+  const dispatch = useAppDispatch();
   return (
     <div>
       {todos.map((e) => (
@@ -16,7 +18,12 @@ export const Todos = () => {
             <b>{e.id}</b>
           </h3>
           <h2>{e.text}</h2>
-          <button style={{ cursor: 'pointer' }}>X</button>
+          <button
+            style={{ cursor: 'pointer' }}
+            onClick={() => dispatch(removeTodos(e.id))}
+          >
+            X
+          </button>
         </div>
       ))}
     </div>
