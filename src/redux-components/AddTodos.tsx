dafb894d11bade8132594e4671272a6a1bc0a7ce
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-31 09:40:19 +0600

    added the updating text collection

--- src/redux-components/AddTodos.tsx
+++ src/redux-components/AddTodos.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { addTodos, updateTodos } from '../redux/slices/todos/todosSlice';
 import { isNotClicked } from '../redux/slices/editButton/editSlice';
 
 const AddTodos = () => {
   const [inputValue, setInputValue] = useState<string>('');
+  const [updatingInputValue, setUpdatingInputValue] = useState('');
+
   const dispatch = useAppDispatch();
+  const todos = useAppSelector((s) => s.todos.todos);
   const editBtnState = useAppSelector((state) => state.editBtn.value);
+  const updatingId = useAppSelector((s) => s.UpdatingId.id);
+
+  let updatingText = todos.find((s) => s.id == updatingId);
+
+  console.log(updatingText);
+
+  useEffect(() => {
+    if (updatingId) {
+      setUpdatingInputValue(`${updatingText?.text}`);
+    }
+  }, [updatingId]);
 
   // const updatingTodos = useAppSelector(state=> state.todos.todos.map(e=> e.id ))
 
@@ -17,13 +31,13 @@ const AddTodos = () => {
           onSubmit={(e) => {
             e.preventDefault();
             dispatch(updateTodos(inputValue));
-            setInputValue('');
+            setUpdatingInputValue('');
           }}
         >
           <input
             type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+            value={updatingInputValue}
+            onChange={(e) => setUpdatingInputValue(e.target.value)}
             required
           />
           <button type="submit" onClick={() => dispatch(isNotClicked())}>
