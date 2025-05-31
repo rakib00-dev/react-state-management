7ec640a19d757154136e735acca87a9fcce98d67
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-31 08:53:43 +0600

    updated the onclick slice

--- src/redux/slices/editButton/editSlice.ts
+++ src/redux/slices/editButton/editSlice.ts
@@ -1,14 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const editButtonSlice = createSlice({
-  initialState: false,
+  initialState: { value: false },
   name: 'editButton',
   reducers: {
     isClicked: (state) => {
-      state = true;
+      state.value = true;
+    },
+    isNotClicked: (state) => {
+      state.value = false;
     },
   },
 });
 
-export const { isClicked } = editButtonSlice.actions;
+export const { isClicked, isNotClicked } = editButtonSlice.actions;
 export default editButtonSlice.reducer;
